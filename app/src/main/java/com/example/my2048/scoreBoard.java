52825bc874b61108c3558e62b2348cb7397e4a33
378984108 <378984108@qq.com>
2020-06-06 22:37:47 +0800

    Project complete

---
+++ app/src/main/java/com/example/my2048/scoreBoard.java
@@ -0,0 +1,13 @@
+package com.example.my2048;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class scoreBoard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score_board);
+    }
+}
